cf678f490feaffe6e4d0346537e1ab95a77279fd
Michael <michael.elliston95@gmail.com>
2025-09-30 09:34:37 -0500

    second commit

--- src/main/java/com/pluralsight/HighScoreWins.java
+++ src/main/java/com/pluralsight/HighScoreWins.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class HighScoreWins {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Please enter team names and scores in the following format: Home:Visitor|73:12 ");
+        String input = myScanner.nextLine();
+
+
+    }
 }
